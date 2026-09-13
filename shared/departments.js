639770c3a41b6f069081cfaa1a697204c4505ed7
aaf3569 <abdullahalsayegh3569@gmail.com>
2026-09-13 19:56:29 +0300

    Make French elective an optional second teacher subject

--- shared/departments.js
+++ shared/departments.js
@@ -162,7 +162,6 @@ const DEPARTMENT_SPECIALIZATIONS = Object.freeze({
   [DEPARTMENTS.GEOGRAPHY_HISTORY]:   ['الجغرافيا', 'التاريخ', 'الاجتماعيات', 'الدستور'],
   [DEPARTMENTS.PHILOSOPHY_SCIENCES]: ['الدستور', 'علم النفس', 'الفلسفة', 'الاجتماعيات'],
   [DEPARTMENTS.MATH]:                ['الرياضيات', 'الإحصاء'],
-  [DEPARTMENTS.FRENCH]:              ['اللغة الفرنسية', 'اللغة الفرنسية (اختيار حرّ)'],
 });
 
 /**
